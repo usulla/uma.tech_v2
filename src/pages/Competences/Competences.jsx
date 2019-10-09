8b73818a470239e54de6702daaa2c7f9ec9b5ba8
msukhareva <msukhareva@a-mos2-400wj1gj.local>
2019-10-09 15:37:27 +0300

    Added page Competencies

--- src/pages/Competences/Competences.jsx
+++ src/pages/Competences/Competences.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import SlideDownItem from "../../components/SlideDownItem/SlideDownItem.jsx";
 import "./competences.scss";
 import competition_bg from "../../images/competition_page/competition_bg@2x.jpg";
+import competition_bg_mob from "../../images/competition_page/competition_bg-mob@2x.jpg";
 
 const Competences = ({ isMobile, dataCompetences }) => {
+  var item1 = {
+    title: dataCompetences[2].item_2,
+    description: dataCompetences[2].item_1.concat(dataCompetences[2].item_3)
+  };
   return (
     <div className="page competences__wrap">
       <div className="competences__content">
@@ -19,20 +25,27 @@ const Competences = ({ isMobile, dataCompetences }) => {
                 <h3>{dataCompetences[0].title2}</h3>
                 <p>{dataCompetences[0].item_1}</p>
               </div>
-              <div className="competences__item--right">
-                <p>
-                  <span className="title">
-                    {dataCompetences[0].item_3.title}
-                  </span>
-                  <span>{dataCompetences[0].item_3.description}</span>
-                </p>
-                <p>
-                  <span className="title">
-                    {dataCompetences[0].item_4.title}
-                  </span>
-                  {dataCompetences[0].item_4.description}
-                </p>
-              </div>
+              {!isMobile && (
+                <div className="competences__item--right">
+                  <p>
+                    <span className="title">
+                      {dataCompetences[0].item_2.title}
+                    </span>
+                    <span>{dataCompetences[0].item_2.description}</span>
+                  </p>
+                  <p>
+                    <span className="title">
+                      {dataCompetences[0].item_3.title}
+                    </span>
+                    {dataCompetences[0].item_3.description}
+                  </p>
+                </div>
+              )}
+              {isMobile && (
+                <SlideDownItem
+                  items={[dataCompetences[0].item_2, dataCompetences[0].item_3]}
+                />
+              )}
             </div>
           </div>
           {/* 2 */}
@@ -40,55 +53,86 @@ const Competences = ({ isMobile, dataCompetences }) => {
             <div className="container">
               <h2>{dataCompetences[1].title1}</h2>
               <h3>{dataCompetences[1].title2}</h3>
-              <div className="competences__item--content">
-                <div className="competences__item--top">
-                  <span className="title col-1">{dataCompetences[1].item_1}</span>
-                  <span className="title col-2">
-                    {dataCompetences[1].item_3}
-                  </span>
+              {!isMobile && (
+                <div className="competences__item--content">
+                  <div className="competences__item--top">
+                    <span className="title col-1">
+                      {dataCompetences[1].item_1}
+                    </span>
+                    <span className="title col-2">
+                      {dataCompetences[1].item_3}
+                    </span>
+                  </div>
+                  <div className="competences__item--down">
+                    <span className="col-1">{dataCompetences[1].item_2}</span>
+                    <span className="col-2">{dataCompetences[1].item_4}</span>
+                  </div>
                 </div>
-                <div className="competences__item--down">
-                  <span className="col-1">{dataCompetences[1].item_2}</span>
-                  <span className="col-2">{dataCompetences[1].item_4}</span>
+              )}
+              {isMobile && (
+                <div className="competences__item--content">
+                  <div className="competences__item--top">
+                    <span className="title col-1">
+                      {dataCompetences[1].item_1}
+                    </span>
+                    <span className="col-1">{dataCompetences[1].item_2}</span>
+                  </div>
+                  <div className="competences__item--down">
+                    <span className="title col-2">
+                      {dataCompetences[1].item_3}
+                    </span>
+                    <span className="col-2">{dataCompetences[1].item_4}</span>
+                  </div>
                 </div>
+              )}
             </div>
           </div>
-          </div>
 
-
-          <div className="competition__image"> 
-            <img src={competition_bg}/>
+          <div className="competition__image">
+            {!isMobile && <img src={competition_bg} />}
+            {isMobile && <img src={competition_bg_mob} />}
           </div>
           {/* 3 */}
           <div className="competences__item competences__item-3 competences__item--white">
             <div className="container">
               <h2>{dataCompetences[2].title1}</h2>
-              <div className="competences__item--content">
-                <div className="competences__item--top">
-                <h3 className="col-1">{dataCompetences[2].title2}</h3>
-                <p className="title2 col-2">
-                  <span dangerouslySetInnerHTML={{__html:(
-                      dataCompetences[2].item_2)}}>
-                  </span>
-                  </p>
+
+              {!isMobile && (
+                <div className="competences__item--content">
+                  <div className="competences__item--top">
+                    <h3 className="col-1">{dataCompetences[2].title2}</h3>
+
+                    <p className="title2 col-2">
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: dataCompetences[2].item_2
+                        }}
+                      ></span>
+                    </p>
+                  </div>
+                  <div className="competences__item--down">
+                    <p className="col-1">
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: dataCompetences[2].item_1
+                        }}
+                      ></span>
+                    </p>
+                    <p className="col-2">
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: dataCompetences[2].item_3
+                        }}
+                      ></span>
+                    </p>
+                  </div>
                 </div>
-                <div className="competences__item--down">
-                  <p className="col-1">
-                  <span dangerouslySetInnerHTML={{__html:(
-                      dataCompetences[2].item_1)}}>
-                  </span>
-                </p>
-                  <p className="col-2">
-                    <span dangerouslySetInnerHTML={{__html:(
-                      dataCompetences[2].item_3)}}>
-                    </span>
-                  </p>
-                </div>
+              )}
+              {isMobile && <SlideDownItem items={[item1]} />}
             </div>
           </div>
-          </div>
-    </div>
-    </div>
+        </div>
+      </div>
     </div>
   );
 };
